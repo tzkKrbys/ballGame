e67a64bfcf029dad14bb92f95e97891146c49be5
tzkkrbys <tzaugment@gmail.com>
2016-03-15 10:07:43 +0900

    一旦形になった

--- js/main.js
+++ js/main.js
@@ -24,8 +24,8 @@ window.onload = function() {
     var rectSpeedY; //移動速度
     var rectX; //X軸の位置
     var rectY; //y軸の位置
-    var rectWidth = 100; //y軸の位置
-    var rectHeight = 10; //y軸の位置
+    var rectWidth = 100; //
+    var rectHeight = 10; //
     //ブロック
     var blocksMarginTop = 60;
 
@@ -34,95 +34,22 @@ window.onload = function() {
 
     var itemsAry = [];
 
-    if(localStorage.getItem('bestTime')){
-        bestTime = localStorage.getItem('bestTime');
-    } else {
-        bestTime = 0;
-    }
-    // console.log(bestTime);
+    var img = new Image();
+    var logoWidth = 25;
+    var logoHeight = 25;
+    img.src = "./images/gsacLogoBlue50x50.png";
 
-    moveLeft = false;
-    moveRight = false;
-    // _isRunning = false;
-    //----------------------------------------ボールの変数
-    // speedX = 4; //移動速度
-    speedY = -4.0; //移動速度
-    speedX = Math.sqrt(Math.pow(speed,2) - Math.pow(speedY,2));
-    // BallX = 50; //X軸の位置
-    // BallY = 350; //y軸の位置
-
-    //----------------------------------------実機の変数
-    rectSpeedX = 8; //移動速度
-    rectSpeedY = 1.3; //移動速度
-    rectX = 0; //X軸の位置
-    rectY = 450; //y軸の位置
-
+    var itemTime = 0;
 
     //----------------------sound
-    select06 =      document.querySelector('#select06');
-    button04a =     document.querySelector('#button04a');
-    destruction1 =  document.querySelector('#destruction1');
-    button05 =      document.querySelector('#button05');
-    bgm =           document.querySelector('#bgm');
-    fanfare =       document.querySelector('#fanfare');
-
-    //----------------------------------------イベントの設定
-    window.addEventListener('keydown', KeyDown, true); //キーを押した時、呼び出される関数を指定
-    window.addEventListener('keyup', KeyUp, true); //キーを離した時、呼び出される関数を指定
-    // document.querySelector('canvas').addEventListener('click', loop, true); //canvasをclickすると、loop関数がスタートする
-    document.querySelector('canvas').addEventListener('click', function(){
-        if(!isGameStarted){
-            isGameStarted = true;
-            gameStartFunc();
-        }
-    }, true); //canvasをclickすると、loop関数がスタートする
-    // fanfare.play();
-    function KeyDown(e) {
-        switch (e.keyCode) {
-            case 37: // ←キー
-                moveLeft = true;
-                break;
-            case 39: // →キー
-                moveRight = true;
-                break;
-            case 65: // Aキー
-        			moveLeft = true;
-        		break;
-        	case 68: // Dキー
-        			moveRight = true;
-        		break;
-            default:
-                break;
-        }
-    }
-
-    function KeyUp(e) {
-        switch (e.keyCode) {
-            case 37: // ←キー
-                moveLeft = false;
-                break;
-            case 39: // →キー
-                moveRight = false;
-                break;
-            case 65: // Aキー
-        			moveLeft = false;
-        		break;
-        	case 68: // Dキー
-        			moveRight = false;
-        		break;
-            default:
-                break;
-        }
-    }
-
-
-
-
-    /* フォントスタイルを定義 */
-    ctx.font = "40px 'ＭＳ Ｐゴシック'";
-    ctx.textAlign = "center";
-    ctx.fillStyle = "#0066bb";
-    ctx.fillText("click!", canvas.width / 2, 250);
+    var select06 =      document.querySelector('#select06');
+    var button04a =     document.querySelector('#button04a');
+    var destruction1 =  document.querySelector('#destruction1');
+    var button05 =      document.querySelector('#button05');
+    var bgm =           document.querySelector('#bgm');
+    var fanfare =       document.querySelector('#fanfare');
+    var itemGet =       document.querySelector('#itemGet');
+    var powpowpow =     document.querySelector('#powpowpow');
 
     //ブロックを配置
     var blockAry = [
@@ -153,6 +80,102 @@ window.onload = function() {
     }
     initblocksColor();
 
+    function Item(x, y, speed) {
+        this.x = 0;
+        this.y = 0;
+        this.speed = 0;
+        this.alive = false;
+        this.setPosition(x, y, speed);
+    }
+    Item.prototype.setPosition = function (x, y, speed){
+        this.x = x;
+        this.y = y;
+        this.speed = speed;
+        this.alive = true;
+    }
+    Item.prototype.move = function (){
+        this.y += this.speed;
+    }
+
+
+    if(localStorage.getItem('bestTime')){
+        bestTime = localStorage.getItem('bestTime');
+    } else {
+        bestTime = 0;
+    }
+    // console.log(bestTime);
+
+    moveLeft = false;
+    moveRight = false;
+    // _isRunning = false;
+    //----------------------------------------ボールの変数
+    // speedX = 4; //移動速度
+    speedY = -4.0; //移動速度
+    speedX = Math.sqrt(Math.pow(speed,2) - Math.pow(speedY,2));
+    // BallX = 50; //X軸の位置
+    // BallY = 350; //y軸の位置
+
+    //----------------------------------------実機の変数
+    rectSpeedX = 8; //移動速度
+    rectSpeedY = 1.3; //移動速度
+    rectX = ctx.canvas.width/2 - rectWidth/2; //X軸の位置
+    rectY = 450; //y軸の位置
+
+    //----------------------------------------イベントの設定
+    window.addEventListener('keydown', KeyDown, true); //キーを押した時、呼び出される関数を指定
+    window.addEventListener('keyup', KeyUp, true); //キーを離した時、呼び出される関数を指定
+    document.querySelector('canvas').addEventListener('click', gameStartFunc, true); //canvasをclickすると、loop関数がスタートする
+    // fanfare.play();
+    function KeyDown(e) {
+        switch (e.keyCode) {
+            case 37: // ←キー
+                moveLeft = true;
+                break;
+            case 39: // →キー
+                moveRight = true;
+                break;
+            case 65: // Aキー
+        		moveLeft = true;
+        		break;
+        	case 68: // Dキー
+        		moveRight = true;
+        		break;
+        	case 13: // enterキー
+                gameStartFunc();
+                break;
+        	case 32: // spaceキー
+                gameStartFunc();
+        		break;
+            default:
+                break;
+        }
+    }
+
+    function KeyUp(e) {
+        switch (e.keyCode) {
+            case 37: // ←キー
+                moveLeft = false;
+                break;
+            case 39: // →キー
+                moveRight = false;
+                break;
+            case 65: // Aキー
+        			moveLeft = false;
+        		break;
+        	case 68: // Dキー
+        			moveRight = false;
+        		break;
+            default:
+                break;
+        }
+    }
+
+    /* フォントスタイルを定義 */
+    ctx.font = "40px 'ＭＳ Ｐゴシック'";
+    ctx.textAlign = "center";
+    ctx.fillStyle = "#0066bb";
+    ctx.fillText("click!", canvas.width / 2, 250);
+
     //ブロックを描画する関数
     function blockDrow(){
         for(var i = 0; i < blockAry.length; i++){
@@ -166,25 +189,7 @@ window.onload = function() {
             }
         }
     }
-    // function itemFall(x) {
-    //     alert(x);
-    // }
-    function Item(x, y, speed) {
-        this.x = 0;
-        this.y = 0;
-        this.speed = 0;
-        this.alive = true;
-        this.setPosition(x, y, speed);
-    }
-    Item.prototype.setPosition = function (x, y, speed){
-        this.x = x;
-        this.y = y;
-        this.speed = speed;
-        // this.alive = true;
-    }
-    Item.prototype.move = function (){
-        this.y += this.speed;
-    }
+
     //ブロックとの当たり判定関数
     function blockHitJudgement(){
         var countBlock = 0;//現在のブロック個数をカウントする変数
@@ -258,8 +263,8 @@ window.onload = function() {
             ctx.fillText("game clear!!", canvas.width / 2, 250);
             if(isGameStarted){
                 isGameStarted = false;//ゲーム中のフラグを下す
-                // bgm.pause();//BGMを停止
-                // bgm.currentTime = 0;//BGM再生タイムを先頭へ
+                bgm.pause();//BGMを停止
+                bgm.currentTime = 0;//BGM再生タイムを先頭へ
                 fanfare.pause();
                 fanfare.currentTime = 0;
                 fanfare.play();
@@ -267,7 +272,6 @@ window.onload = function() {
                     bestTime = countTime;
                     localStorage.setItem('bestTime', bestTime);
                 }
-                // animationStop();
             }
         }
     }
@@ -275,35 +279,33 @@ window.onload = function() {
     //関数群
     //初期化関数
     function gameStartFunc() {
-        count = 0;
-        moveLeft = false;
-        moveRight = false;
-        blockAry = [
-            [1,1,1,1,1,1,1,1],
-            [1,1,1,1,1,1,1,1],
-            [1,1,1,1,1,1,1,1],
-            [2,2,2,2,2,2,2,2]
-        ];
-        //----------------------------------------ボールの変数
-        // speedY = -4.0; //移動速度
-        speedY = -(Math.floor(Math.random() * 2.0) + 2 ); //移動速度
-        speedX = Math.sqrt(Math.pow(speed,2) - Math.pow(speedY,2));
-        if(Math.random() > 0.5){
-            speedX = -speedX;
+        if(!isGameStarted){
+            isGameStarted = true;
+            count = 0;
+            moveLeft = false;
+            moveRight = false;
+            blockAry = [
+                [2,2,1,1,2,1,1,2],
+                [1,1,2,1,1,2,1,2],
+                [2,1,2,1,1,2,2,1],
+                [2,2,2,2,2,2,2,2]
+            ];
+            itemsAry = [];
+            itemTime = 0;
+            rectWidth = 100;
+            rectHeight = 10;
+            //----------------------------------------ボールの変数
+            speedY = -(Math.random() * 2 + 3 ); //移動速度
+            speedX = Math.sqrt(Math.pow(speed,2) - Math.pow(speedY,2));
+            if(Math.random() > 0.5){
+                speedX = -speedX;
+            }
+            //BGMスタート
+            bgm.pause();
+            bgm.currentTime = 0;
+            bgm.play();
         }
-        //BGMスタート
-        // bgm.pause();
-        // bgm.currentTime = 0;
-        // bgm.play();
     }
-    // init(); //一旦値を初期化する
-
-    // function drawCircle(x, y, scale, color) {
-    //     ctx.beginPath();
-    //     ctx.arc(x, y, scale, 0, 2 * Math.PI, false);
-    //     ctx.fillStyle = color;
-    //     ctx.fill();
-    // }
 
     function drawRect(x, y, width, height, color) {
         ctx.beginPath();
@@ -328,26 +330,24 @@ window.onload = function() {
         cancelAnimationFrame(_animationID);
     }
 
-    // loop();
-
-
-
-    // img.onload = function() {
-    //     ctx.drawImage(img, 50, 50);
-    //     // ctx.drawImage(img, 0, 0, 50, 50);
-    // };
-    var img = new Image();
-    var logoWidth = 25;
-    var logoHeight = 25;
-    img.src = "./images/gsacLogoBlue50x50.png";
-
-
     // -----------------------------------------------------------ループ関数
     //この関数内でいろんな値をupdateさせて、アニメーションさせる
     function loop() {
         _animationID = requestAnimFrame(loop);
         ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);//canvasをクリア
         blockDrow();//ブロックを描画
+
+        if(itemTime){
+            itemTime--;
+            if(itemTime === 1){
+                //元に戻す
+                rectWidth = 100;
+                rectHeight = 10;
+                powpowpow.pause();
+                powpowpow.currentTime = 0;
+                powpowpow.play();
+            }
+        }
 
         if(isGameStarted){
             count++;
@@ -356,21 +356,20 @@ window.onload = function() {
             BallY += speedY;
             // 円を描画
             // 変数xの値を変化させる
+            var hitWall = false;
             if (BallX < logoWidth/2 && speedX < 0) {
                 speedX = -speedX;
-                button04a.pause();
-                button04a.currentTime = 0;
-                button04a.play();
+                hitWall = true;
             }
             if (ctx.canvas.width - BallX < logoWidth/2 && speedX > 0) {
                 speedX = -speedX;
-                button04a.pause();
-                button04a.currentTime = 0;
-                button04a.play();
+                hitWall = true;
             }
             // 変数yの値を変化させる
-            if (BallY < 5) {
+            if (BallY < logoHeight/5) {
                 speedY = -speedY;
+            }
+            if(hitWall){
                 button04a.pause();
                 button04a.currentTime = 0;
                 button04a.play();
@@ -388,50 +387,75 @@ window.onload = function() {
                 ctx.fillStyle = "#0066bb";
                 ctx.textAlign = "center";
                 ctx.fillText("game over", canvas.width / 2, 250);
-                // bgm.pause();
-                // bgm.currentTime = 0;
+                bgm.pause();
+                bgm.currentTime = 0;
                 isGameStarted = false;
-                // animationStop(_animationID);
+                itemsAry = [];
+                itemTime = 0;
+                rectWidth = 100;
+                rectHeight = 10;
+                rectX = ctx.canvas.width/2 - rectWidth/2; //X軸の位置
             }
             //----------------------------------------------------------item
             if(itemsAry.length) {
                 for(var i = 0; i < itemsAry.length; i++){
-                    itemsAry[i].move();
-                    ctx.drawImage(img, itemsAry[i].x - 25/2, itemsAry[i].y - 25/2, 25, 25);
-                    //自機との当たり判定
-                    if (rectY - (itemsAry[i].y + 25/2) < 0 &&
-                        (rectY + rectHeight) - (itemsAry[i].y + 25/2) > 0 ) {
-                        if (itemsAry[i].x > rectX && itemsAry[i].x < (rectX + rectWidth)) {
-                            itemsAry.splice(i, 1);
-                            rectWidth += 40;
-                            rectX -= 20;
-                            // animationStop();
-                            // select06.pause();
-                            // select06.currentTime = 0;
-                            // select06.play();
+                    if(itemsAry[i].alive){
+                        itemsAry[i].move();
+                        ctx.drawImage(img, itemsAry[i].x - 25/2, itemsAry[i].y - 25/2, 25, 25);
+                        //自機との当たり判定
+                        if (rectY - (itemsAry[i].y + 25/2) < 0 &&
+                            (rectY + rectHeight) - (itemsAry[i].y - 25/2) > 0 ) {
+                            if (itemsAry[i].x + 25/2 > rectX && itemsAry[i].x - 25/2 < (rectX + rectWidth)) {
+                                itemsAry[i].alive = false;
+                                if(!itemTime){
+                                    itemTime = 300;
+                                    rectWidth += 40;
+                                    rectX -= 20;
+                                }else{
+                                    itemTime = 300;
+                                }
+                                itemGet.pause();
+                                itemGet.currentTime = 0;
+                                itemGet.play();
+                            }
+                        }else if(itemsAry[i].y > ctx.canvas.height + 25/2){//画面外に出た場合、配列から削除する
+                            // itemsAry.splice(i, 1);
+                            itemsAry[i].alive = false;
                         }
-                    }else if(itemsAry[i].y > ctx.canvas.height + 25/2){//画面外に出た場合、配列から削除する
-                        itemsAry.splice(i, 1);
+
                     }
                 }
             }
 
             //自機との当たり判定
-            // if (BallY + logoHeight/2 > rectY && BallY < rectY + rectHeight && speedY > 0) {
-            if (rectY - BallY < logoHeight/2 &&
-                (rectY + rectHeight) - BallY > logoHeight/2 &&
+            var hit = false;
+            if (rectY - (BallY + logoHeight/2 ) < 0 &&
+                (rectY + rectHeight) - ( BallY - logoHeight/2 ) > 0 &&
                 speedY > 0) {
-                if (BallX > rectX && BallX < (rectX + rectWidth)) {
+                if (BallX >= rectX && BallX <= (rectX + rectWidth)) {
                     if(moveLeft){
                         speedX = speedX - 1;
                     } else if (moveRight) {
                         speedX = speedX + 1;
                     }
                     if(speedX >= 6){
-                        speedX = 5.9;
+                        speedX = 5.4;
                     }else if(speedX <= -6){
-                        speedX = -5.9;
+                        speedX = -5.4;
                     }
+                    hit = true;
+                } else if (BallX < rectX ){
+                    if(BallX + logoWidth/2 > rectX ){
+                        speedX = -5.4;
+                        hit = true;
+                    }
+                } else if ( BallX > (rectX + rectWidth) ){
+                    if(BallX - logoWidth/2 < (rectX + rectWidth)){
+                        speedX = 5.4;
+                        hit = true;
+                    }
+                }
+                if(hit){//当たってたら
                     speedY = Math.sqrt(Math.pow(speed,2) - Math.pow(speedX,2));
                     speedY = -speedY;
                     select06.pause();
@@ -439,14 +463,16 @@ window.onload = function() {
                     select06.play();
                 }
             }
-        } else {//ゲーム中で
+        } else {//ゲーム中でない時
             BallX = rectX + rectWidth/2;
             BallY = rectY - logoHeight/2;
+            /* フォントスタイルを定義 */
+            ctx.font = "40px 'ＭＳ Ｐゴシック'";
+            ctx.textAlign = "center";
+            ctx.fillStyle = "#0066bb";
+            ctx.fillText("click or enter!", canvas.width / 2, 250);
         }
         ctx.drawImage(img, BallX - logoWidth / 2, BallY - logoHeight / 2, 25, 25);
-
-
-        // drawCircle(BallX, BallY, 6, '#60A869'); //関数を実行し、変化した変数を引数に渡して円を描画する
 
         //自機の変数の値を変化させる
         if (moveLeft) {//もし左キーが押されていたら
@@ -461,9 +487,13 @@ window.onload = function() {
             rectX = ctx.canvas.width - rectWidth;
         }
 
-        // lastPosition = rectX;
-
-        drawRect(rectX, rectY, rectWidth, rectHeight, "#294");//自機を描画する
+        var color;
+        if(itemTime){
+            color = "#84f";
+        }else{
+            color = "#294";
+        }
+        drawRect(rectX, rectY, rectWidth, rectHeight, color);//自機を描画する
 
         blockHitJudgement();
         countTime = Math.floor(count / 60);
